88297fe7056badd22a7c8ed8d2c22c08db4f21d6
Sarvesh <atsarveshyadav@gmail.com>
2025-06-26 20:40:51 +0530

    Display key presses and turn off ctrl-c & ctrl-z

--- text_editor.c
+++ text_editor.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -15,7 +17,7 @@ void enableRawMode(){
 
     struct termios raw = orig_termios;
 
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -25,9 +27,13 @@ int main()
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1)==1 && c !='q');
+    while (read(STDIN_FILENO, &c, 1)==1 && c !='q')
     {
-        /* code */
+        if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
     }
     
     return 0;
